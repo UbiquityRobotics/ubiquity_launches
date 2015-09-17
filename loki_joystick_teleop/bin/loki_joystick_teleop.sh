23a2b8437a3e7e0b3c2283f1f55d224236129621
Wayne Gramlich <wayne@gramlich.net>
2015-09-17 04:14:45 +0000

    Finished cleaing up loki_robot except for documentation.

--- loki_joystick_teleop/bin/loki_joystick_teleop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export ROBOT_BASE=loki
-roslaunch ubiquity_launches m_joystick_teleop.launch
--- loki_local_costmap/bin/loki_local_costmap.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export ROBOT_BASE=loki
-roslaunch ubiquity_launches m_fiducial_slam.launch.xml
