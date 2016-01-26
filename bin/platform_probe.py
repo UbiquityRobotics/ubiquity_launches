db287f21de2468601dfd88acd34e1ec983fd17b3
Wayne Gramlich <wayne@gramlich.net>
2016-01-25 22:43:22 -0800

    Added platform_probe.py and roscore_start.sh to ubiquity_launche/bin

---
+++ bin/platform_probe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+
+def main():
+    is_raspberry_pi = os.path.exists("/dev/ttyAMA0")
+    #print("is_raspberry_pi={0}".format(is_raspberry_pi))
+    has_usb_serial = os.path.exists("/dev/ttyUSB0")
+    #print("has_usb_serial={0}".format(has_usb_serial))
+
+    # This is really kudgey (technical term) for now:
+    platform = "stage"
+    if is_raspberry_pi:
+	platform = "loki"
+	if has_usb_serial:
+	    platform = "magni"
+    print("platform:={0}".format(platform))
+
+if __name__ == "__main__":
+    main()
---
+++ robot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+
+rospy.init_node("robot_loop")
+
+while True:
+    print("robot")
+    time.sleep(1)
