7f50d7cd861ea2430378eca8fd5d173f2ae1e650
Wayne C. Gramlich <Wayne@Gramlich.Net>
2016-02-01 23:24:07 -0800

    Updated test.launch and urtest.sh

--- bin/urtest.sh
+++ bin/urtest.sh
@@ -4,8 +4,18 @@
 ROBOT_HOST=`echo $ROS_MASTER_URI | sed -e s,http://,, | sed -e s,:11311,,`
 #echo ROBOT_HOST=$ROBOT_HOST
 
-# Probe to find out what kind of platform the robot is:
-PLATFORM=`ssh ubuntu@$ROBOT_HOST /home/ubuntu/catkin_ws/src/ubiquity_launches/bin/platform_probe.py`
+# This chunk of code probes the ROBOT_HOST to find out about its
+# platform.  This is done copying short script over to
+# ROBOT_HOST:/tmp/platform.sh, setting the execute bit for it, and
+# using `ssh` to remotely execute it:
+cat >/tmp/platform.sh<<EOF
+#!/bin/bash
+source /home/ubuntu/.profile
+rosrun ubiquity_launches platform_probe.py
+EOF
+scp -q /tmp/platform.sh ubuntu@$ROBOT_HOST:/tmp
+ssh ubuntu@$ROBOT_HOST chmod +x /tmp/platform.sh
+PLATFORM=`ssh ubuntu@$ROBOT_HOST /tmp/platform.sh`
 #echo PLATFORM=$PLATFORM
 
 # Clear out file we use to indicate when the X11 channel is up:
@@ -16,9 +26,11 @@ rm -f /tmp/x11_up
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
-# Now open the X11 channel:
+# Now open the X11 channel using '&' to put it into the background.
+# The last command will sleep for a million seconds to keep the channel
+# open for a while:
 (ssh -Y ubuntu@$ROBOT_HOST \
-  'echo export DISPLAY=$DISPLAY > /tmp/display.sh ; echo $DISPLAY; sleep 1000000' \
+  'echo $DISPLAY > /tmp/display ; echo $DISPLAY; sleep 1000000' \
   1>&2) 2>/tmp/x11up &
 
 # Now we wait for something to be written into `/tmp/x11up` before
@@ -26,6 +38,18 @@ trap "kill 0" EXIT
 while [ ! -s /tmp/x11up ] ;	\
     do sleep 1; 		\
     done
+
+# Lastly, we need to create an executable file called /tmp/env_loader.sh
+# over on ROBOT_HOST.  This file slurps in the ROS environment variables
+# from `~/.profile` and sets the `DISPLAY` environment variable:
+cat >/tmp/env_loader.sh<<EOF
+#!/bin/bash
+source ~/.profile
+export DISPLAY=\`cat /tmp/display\`
+exec "\$@"
+EOF
+scp -q /tmp/env_loader.sh ubuntu@$ROBOT_HOST:/tmp
+ssh ubuntu@$ROBOT_HOST 'chmod +x /tmp/env_loader.sh'
 
 # Now we can run the roslaunch command specifying both the PLATFORM
 # and ROBOT_HOST:
